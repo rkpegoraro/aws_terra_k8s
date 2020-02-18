0c69d16212113b8992f1d4a21567bc50506ade63
ricardo.pegoraro <ricardo.pegoraro@tesouro.gov.br>
2020-02-18 09:53:20 -0300

    changed instance name reference

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 #   public_key = "${file("terraform-demo.pub")}"
 # }
 
-resource "aws_instance" "my-instance" {
+resource "aws_instance" "k8s-masters" {
   count         = "3"
   ami           = "ami-0b51ab7c28f4bf5a6" //Ubuntu 18_04
   instance_type = "t2.micro"
