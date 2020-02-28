fcedd3a7ecf6dddc1d10302f44d50574ddda2dc1
ricardo.pegoraro <ricardo.pegoraro@tesouro.gov.br>
2020-02-28 14:48:43 -0300

    Haproxy installed and running

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,9 +5,15 @@ terraform {
 provider "aws" {
   region = "us-east-2" //Ohio
 
-  # # Locks AWS provider version
-  # version = "~> 2.49"
+  # Locks AWS provider version
+  version = "~> 2.50"
 }
+
+provider "null" {
+  # Locks Null provider version
+  version = "~> 2.1"
+}
+
 
 # # VPC
 # resource "aws_vpc" "k8s-vpc" {
@@ -55,6 +61,14 @@ resource "aws_security_group" "k8s_multimaster" {
     self      = true
   }
 
+  # ICMP (ping) from anywhere
+  ingress {
+    from_port = 8
+    to_port = 0
+    protocol = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   # Allow all outbound requests
   egress {
     from_port   = 0
@@ -67,8 +81,8 @@ resource "aws_security_group" "k8s_multimaster" {
 # Proxy Instance
 resource "aws_instance" "k8s_proxy" {
 
-  ami                    = "ami-0b51ab7c28f4bf5a6" //Ubuntu 18_04
-  instance_type          = "t2.micro"
+  ami                    = var.ami
+  instance_type          = var.instance_type["proxy"]
   key_name               = "k8s-test"
   vpc_security_group_ids = [aws_security_group.k8s_multimaster.id]
   user_data              = <<-EOF
@@ -76,27 +90,6 @@ resource "aws_instance" "k8s_proxy" {
                   sudo hostname "${var.proxy_hostname}"
                   sudo echo "${var.proxy_hostname}" > /etc/hostname
                   sudo apt-get update
-                  sudo apt-get -qq install python -y
-
-                  # sudo apt-get install -y haproxy
-                  # #TODO look for a more elagant way to copy the haproxy.cfg
-                  # # I was not able to copy a local file to /etc/haproxy/haproxy.cfg using the file provisioner
-                  # # I had to copy the file to /tmp them opem another connection with remote-exec to copy the file to the prper place
-                  # # That was too messy
-                  # echo "" | sudo tee -a /etc/haproxy/haproxy.cfg
-                  # echo "frontend kubernetes" | sudo tee -a /etc/haproxy/haproxy.cfg
-                  # echo "    mode tcp" | sudo tee -a /etc/haproxy/haproxy.cfg
-                  # echo "    bind k8s-haproxy-1:6443" | sudo tee -a /etc/haproxy/haproxy.cfg
-                  # echo "    option tcplog" | sudo tee -a /etc/haproxy/haproxy.cfg
-                  # echo "    default_backend k8s-masters" | sudo tee -a /etc/haproxy/haproxy.cfg
-                  # echo "" | sudo tee -a /etc/haproxy/haproxy.cfg
-                  # echo "backend k8s-masters" | sudo tee -a /etc/haproxy/haproxy.cfg
-                  # echo "    mode tcp" | sudo tee -a /etc/haproxy/haproxy.cfg
-                  # echo "    balance roundrobin" | sudo tee -a /etc/haproxy/haproxy.cfg
-                  # echo "    option tcp-check" | sudo tee -a /etc/haproxy/haproxy.cfg
-                  # echo "    server k8s-master-1 k8s-master-1:6443 check fall 3 rise 2" | sudo tee -a /etc/haproxy/haproxy.cfg
-                  # echo "    server k8s-master-2 k8s-master-2:6443 check fall 3 rise 2" | sudo tee -a /etc/haproxy/haproxy.cfg
-                  # echo "    server k8s-master-3 k8s-master-3:6443 check fall 3 rise 2" | sudo tee -a /etc/haproxy/haproxy.cfg
                   EOF
 
   tags = {
@@ -107,9 +100,9 @@ resource "aws_instance" "k8s_proxy" {
 
 # K8s Master Instances
 resource "aws_instance" "k8s_masters" {
-  count                  = "1"
-  ami                    = "ami-0b51ab7c28f4bf5a6" //Ubuntu 18_04
-  instance_type          = "t2.micro"
+  count                  = var.master_count
+  ami                    = var.ami
+  instance_type          = var.instance_type["master"]
   key_name               = "k8s-test"
   vpc_security_group_ids = [aws_security_group.k8s_multimaster.id]
   user_data              = <<-EOF
@@ -117,6 +110,7 @@ resource "aws_instance" "k8s_masters" {
                   sudo hostname "${var.master_prefix}${count.index + 1}"
                   sudo echo "${var.master_prefix}${count.index + 1}" > /etc/hostname
                   sudo apt-get update
+                  #docker install
                   curl -fsSL https://get.docker.com | sh
                   EOF
 
@@ -128,9 +122,9 @@ resource "aws_instance" "k8s_masters" {
 
 # K8s Worker Instances
 resource "aws_instance" "k8s_workers" {
-  count                  = "1"
-  ami                    = "ami-0b51ab7c28f4bf5a6" //Ubuntu 18_04
-  instance_type          = "t2.micro"
+  count                  = var.worker_count
+  ami                    = var.ami
+  instance_type          = var.instance_type["worker"]
   key_name               = "k8s-test"
   vpc_security_group_ids = [aws_security_group.k8s_multimaster.id]
   user_data              = <<-EOF
@@ -146,73 +140,63 @@ resource "aws_instance" "k8s_workers" {
   }
 }
 
+# Consolidate all instances on a single list
 locals {
   # A list of all instances created
   instance_list = concat([aws_instance.k8s_proxy], aws_instance.k8s_masters, aws_instance.k8s_workers)
 }
 
-# # Edit hosts file on all created instances
-# resource "null_resource" "edit_hosts" {
-#   # # Changes to any instance of the cluster requires re-provisioning
-#   # triggers = {
-#   #   cluster_instance_ids = "${join(",", aws_instance.cluster.*.id)}"
-#   # }
+# Edit hosts file on all created instances
+# It is possible to do this with ansible, but I choose to left this as an example
+resource "null_resource" "edit_hosts" {
+  count = length(local.instance_list)
 
-#   count = length(local.instance_list)
-
-#   depends_on = [
-#     aws_instance.k8s_proxy,
-#     aws_instance.k8s_masters,
-#     aws_instance.k8s_workers,
-#   ]
-
-#   connection {
-#     type        = "ssh"
-#     user        = "ubuntu"
-#     private_key = "${file(var.private_key)}"
-#     host        = element(local.instance_list.*.public_ip, count.index)
-#   }
-
-#   provisioner "remote-exec" {
-#     # Change /etc/host file
-#     inline = [
-#       for host in local.instance_list :
-#       "echo ${host.private_ip} ${host.tags.Name} | sudo tee -a /etc/hosts"
-#     ]
-#   }
-# }
-
-variable "names" {
-  description = "A list of names"
-  type        = list(string)
-  default     = ["neo", "trinity", "morpheus"]
-}
-
-# Create local inventory
-resource "null_resource" "ansible_inventory" {
-  
   depends_on = [
     aws_instance.k8s_proxy,
     aws_instance.k8s_masters,
     aws_instance.k8s_workers,
   ]
 
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file(var.private_key)
+    host        = element(local.instance_list.*.public_ip, count.index)
+  }
+
+  provisioner "remote-exec" {
+    # Change /etc/host file
+    inline = [
+      for host in local.instance_list :
+      "echo ${host.private_ip} ${host.tags.Name} | sudo tee -a /etc/hosts"
+    ]
+  }
+}
+
+
+# Create local inventory
+resource "null_resource" "ansible_inventory" {
+  
+  depends_on = [
+    null_resource.edit_hosts,
+  ]
+
   provisioner "local-exec" {
     command = <<-EOT
       #!/bin/bash
-      touch inventory.ini
-      echo "[haproxy]" | tee -a inventory.ini
-      echo "${aws_instance.k8s_proxy.public_ip} | tee -a inventory.ini
-      echo "[k8s_masters]" | tee -a inventory.ini
+      > ../ansible/inventory.ini
+      echo "[haproxy]" | tee -a ../ansible/inventory.ini
+      echo "${aws_instance.k8s_proxy.tags.Name} ansible_host=${aws_instance.k8s_proxy.public_ip} private_ip=${aws_instance.k8s_proxy.private_ip}" | tee -a ../ansible/inventory.ini
+      echo "[k8s_masters]" | tee -a ../ansible/inventory.ini
       %{ for node in aws_instance.k8s_masters }
-        echo ${node.public_ip} | tee -a inventory.ini
+        echo "${node.tags.Name} ansible_host=${node.public_ip} private_ip=${node.private_ip}" | tee -a ../ansible/inventory.ini
       %{ endfor ~}
-      echo "[k8s_workers]" | tee -a inventory.ini
+      echo "[k8s_workers]" | tee -a ../ansible/inventory.ini
       %{ for node in aws_instance.k8s_workers }
-        echo ${node.public_ip} | tee -a inventory.ini
+        echo "${node.tags.Name} ansible_host=${node.public_ip} private_ip=${node.private_ip}" | tee -a ../ansible/inventory.ini
       %{ endfor ~}
       export ANSIBLE_HOST_KEY_CHECKING=False;
-	    #ansible-playbook -u ${var.remote_user} --private-key ${var.private_key} -i inventory.ini ../playbooks/install_haproxy.yml
+      ansible-playbook -i ../ansible/inventory.ini -u ${var.remote_user} --private-key=${var.private_key} ../ansible/playbooks/install_haproxy.yml
     EOT  
   }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,25 @@
 variable "private_key" {
-  # default = "C:\\tools\\keys\\k8s-test.pem"
   default = "~/.ssh/k8s-test.pem"
+}
+
+variable "ami" {
+  default = "ami-0b51ab7c28f4bf5a6" //Ubuntu 18_04
+}
+
+variable "instance_type" {
+  default = {
+    proxy = "t2.micro"
+    master = "t2.micro"
+    worker = "t2.micro"
+  }
+}
+
+variable "master_count" {
+  default = 3
+}
+
+variable "worker_count" {
+  default = 1
 }
 
 variable "remote_user" {
